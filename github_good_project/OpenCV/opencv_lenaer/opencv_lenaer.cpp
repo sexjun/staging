746da52b964cb4bd70dd20688964016f4aadfefb
= <1196586501@qq.com>
2021-09-25 14:26:22 +0800

    add opencv project
    Signed-off-by: = <1196586501@qq.com>

---
+++ github_good_project/OpenCV/opencv_lenaer/opencv_lenaer.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    Mat image_in = imread("1.png");
+    namedWindow("show_imput_image", 1);
+    imshow("show_input_image", image_in);
+    waitKey(0);
+    return 0;
+}
+
---
+++ language_laner/cpp/cpp/cpp.cpp
@@ -0,0 +1,39 @@
+﻿// cpp.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void test02()
+{
+	//替换
+	string str1 = "abcdefgde";
+	str1.replace(1, 3, "1111");
+
+	cout << "str1 = " << str1 << endl;
+}
+
+void test01()
+{
+
+	string str = "abcdefg";
+	string subStr = str.substr(1, 3);
+	cout << "subStr = " << subStr << endl;
+
+	string email = "hello@sina.com";
+	int pos = email.find("@");
+	cout << pos << endl;
+	string username = email.substr(0, pos);
+	string username1 = email.substr(0, 5);
+	cout << "username: " << username << endl;
+	cout << "username1: " << username1 << endl;
+
+}
+
+int main()
+{
+	test01();
+    std::cout << "Hello World!\n";
+}
